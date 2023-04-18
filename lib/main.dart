ee198d9ea67f1546e08f25f137d74ebcf1c62750
sushant-911 <sushant911jha@gmail.com>
2023-04-18 22:45:06 +0530

    added comment in main

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:catalog_app/utils/routes.dart';
 import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
+// main function
 void main() {
   runApp(MyApp());
 }
